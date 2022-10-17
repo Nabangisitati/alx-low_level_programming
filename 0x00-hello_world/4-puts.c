adbb698b86ec127d44beeb568e4871d87020eb22
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-17 14:30:23 -0700

    C program that prints to the standard output using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *Main Program displays output on screen
+ * Returns 0
+ */
+int main(void)
+{
+	char string[] = "Programming is like building a multilingual puzzle \n";
+
+	puts("Programming is like building a multilingual puzzle \n"); 
+	return (0);
+}

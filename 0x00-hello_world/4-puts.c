663ad4b29d86a68a8dcd2c77efa8ed57c2017db3
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-17 15:26:06 -0700

    Corrections to a C program that prints using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- *Main Program displays output on screen
- * Returns 0
+/**
+ *main -Entry point
+ * Program displays output using puts
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle \n"); 
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }

05792622aa0eae47b741fea73d5e6da473203cc5
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-17 15:41:36 -0700

    C program that prints out input using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main -Entrypoint
+ * Program prints output using printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

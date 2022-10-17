99e75552794e30de31dfd32bc8dda4ca4d60e2a8
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-17 16:43:16 -0700

    Print the sizes of types in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Program prints out the sizes of different type
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long int j;
+	long long int k;
+	float u;
+
+	printf("Size of a char:%c\n",sizeof(char));
+	printf("Size of an int:%d\n",sizeof(i));
+	printf("Size of a long int:%ld\n", sizeof(j));
+	printf("Size of a long long int:%lld\n",sizeof(k));
+	printf("Size of a float:%f\n", sizeof(u));
+	return (0);
+}

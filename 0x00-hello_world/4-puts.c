164c8ceb4d757a541e7f9caf1eeb12abc882c2c6
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-17 14:46:31 -0700

    C program that prints out to the stdio using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,7 @@
  */
 int main(void)
 {
-	char string[] = "Programming is like building a multilingual puzzle \n";
 
-	puts("Programming is like building a multilingual puzzle \n"); 
+	puts("\"Programming is like building a multilingual puzzle \n"); 
 	return (0);
 }

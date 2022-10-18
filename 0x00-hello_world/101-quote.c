9ca1819f6380705bddd85cb09e2f4435ad537c00
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-18 05:34:08 -0700

    C program that writes to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * C prog. that writes to the staandard error (stderr)
+ *Return: returns 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}

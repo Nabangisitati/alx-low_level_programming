30da50f910480113196b082a78e6a4032ed4728c
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-19 17:11:10 -0700

    single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main --entry point
+ * prints base 10 single digit numbers
+ * Return: Return 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	if (i >= 0 && i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+

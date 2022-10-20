5ff20689c0bc693a832943d9cb7103bb274e77e1
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-19 18:18:20 -0700

    print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entyr point
+ * print base 10 single digit using putchar
+ * Return: Return 0
+ */
+int main(void)
+{
+	int i = 0;
+	while (i >= 0 && i <= 9)
+	{
+		putchar('i');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

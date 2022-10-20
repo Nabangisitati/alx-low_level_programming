95a14b41b8a0e4d768ca88333327767d91a1a096
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-19 19:28:39 -0700

    print the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,16 @@
  */
 int main(void)
 {
-	print_alphabet();
+	void print_alphabet(void)
+	{
+		int i = 97;
+
+		while (i <= 122)
+		{
+			_putchar(i);
+			i++;
+		}
+	}
 	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,2 @@
 /*Header file for functions*/
 int _putchar(char c);
-void print_alphabet(void);

8553b1d482d612898ef38c0f87abc623c789c943
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-25 16:12:57 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Prints the length of a string
+ * @s: Pointer to a char
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; *s[i] != '\0'; i++)
+	{
+		len = i;
+	}
+	return (len);
+}

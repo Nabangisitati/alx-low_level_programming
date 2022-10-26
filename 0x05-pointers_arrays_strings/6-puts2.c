464d30cc95a063dd3aa899f9d37c22ba7416a573
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-26 02:50:10 -0700

    Print every other string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,19 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-		i += 2;
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
 	}
-	 _putchar('\n');
+	_putchar('\n');
 
 }

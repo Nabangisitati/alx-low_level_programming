ff5bfba5eed1045dd1da56bf95151ac5a9d2df31
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-27 22:37:58 -0700

    Reverse an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses an array of integers
+ * @a: array ot ints
+ * @n: an int
+ */
+
+void reverse_array(int *a, int n)
+{
+	int j, i, k;
+	int b[];
+
+	/*Reverse original array a and assign into into a  new array b[]*/
+	for (j = (n - 1); j >= 0; j--)
+	{
+		for (i = 0; i < n; i++)
+		{
+			b[i] = a[j];
+		}
+	}
+	/*Copy contents of b back into original array a*/
+	for (k = 0; k < n; k++)
+	{
+		a[k] = b[k];
+	}
+}

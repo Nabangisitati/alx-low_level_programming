c8b0f6a50b6ce68c1d78fa9a1a6a34d8c6120232
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-30 07:42:24 -0700

    Print the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 197; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}

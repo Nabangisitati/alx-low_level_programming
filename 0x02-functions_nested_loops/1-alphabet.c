7d6d8f22509bf7ce4ce9c0a6a967d63c7162d09e
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-30 07:17:56 -0700

    print lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,16 @@
 #include "main.h"
-
+/**
+ * print _alphabet - prints lowercase alphabet
+ * Return: void
+ */
 void print_alphabet(void)
 {
 	int i = 97;
+
 	while (i <= 122)
 	{
 		_putchar(i);
 		i++;
 	}
-}
-/**
- * main - checks code
- * program prints alphabet
- * Return: 0 (successful)
- */
-int main(void)
-{
-	print_alphabet();
 	_putchar('\n');
-	return (0);
 }

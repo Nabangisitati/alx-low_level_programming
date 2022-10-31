aa1f0aabf595800e8ee3879f09ebe429769a9aa4
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-30 18:38:11 -0700

    Sum of multiples of 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main - program that  computes and prints the sum of all
+ * multiples of 3 or 5 below 1024
+ * Return:returns 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 5 == 0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("sum of multiples of 5  below 1024 is %d", sum);
+	return (0);
+}

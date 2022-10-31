377102cde617c2629d20db6c78f89e4288a2c1a5
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-30 19:49:01 -0700

    Print a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n:int
+ * Return:void
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}

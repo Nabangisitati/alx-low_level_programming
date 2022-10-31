eaf33936850364164e3e824b5c886137bc4b2741
Phyllis Sitati <nabangisitati@gmail.com>
2022-10-30 18:13:29 -0700

    Natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,20 +13,18 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
-			if (n == 98)
-				printf("%d", i);
 			printf("%d, ", i);
 		}
+		printf("%d", i);
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			if (n == 98)
-				printf("%d", i);
 			printf("%d, ", i);
 		}
+		printf("%d", i);
 	}
 }

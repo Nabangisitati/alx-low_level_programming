12af76eb398ceecf77d32f34e5a838f63736d301
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-16 02:44:23 -0800

    C program that print to the stdio using the puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of program
+ * main - Program entry point
  *
- * Return: 0 (Success)
+ * Return: always 0 (successful)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
+	put("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

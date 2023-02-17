fc7bc831a08fb19a206bfc5e5327774e67220ba5
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-17 06:33:29 -0800

    Print base 10 single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}

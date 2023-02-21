d0b0f9af69b21f314f08635c99b901c627890a8d
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-21 06:36:37 -0800

    Function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+int _putchar(char c);
+
+/**
+ * add - adds two ints a and b
+ * @a: int arg
+ * @b: int arg
+ *
+ * Return: an int result of adding a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

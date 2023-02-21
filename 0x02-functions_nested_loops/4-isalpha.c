e66c78b88baa3b419c0b741f86d3dec1db5f5a63
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-21 12:59:40 -0800

    Pushing the header file main.h

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+int _putchar(char c)
+
+/**
+ * _isalpha - checksfor alphabetic character
+ * @c: character to be checked
+ *
+ * Return: 1 if c is an alphabet 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >=[a - zA - Z])
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

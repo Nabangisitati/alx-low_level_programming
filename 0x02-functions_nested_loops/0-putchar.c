4e7a514b9e437ef5378577c0ec50b887c815a257
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-21 13:29:26 -0800

    Program prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char word[] = "_putchar";
-	int i;
-	int j = 8;
-
-	for (i = 0; i < j i++)
-	{
-		_putchar(word[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }

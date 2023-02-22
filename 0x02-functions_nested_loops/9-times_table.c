c0ea9d0560f31789fe0335aed0435976f5c181b0
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-22 03:59:16 -0800

    Print 9x multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,10 +11,13 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		for (j = 0; j <= 9; j++)
 		{
 			p = j * i;
-			_putchar(p);
+			_putchar((p % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}

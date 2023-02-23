b75cb16114d539a84fc6b0702e5494a6ccbf109b
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-22 18:30:27 -0800

    Print lowercase alpha 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,14 @@
  */
 void print_alphabet_x10(void)
 {
-	char i = 'a';
+	char i;
 	int j;
 
 	for (j = 0; j < 10; j++)
 	{
-		while (i <= 'z')
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
-			i++;
 		}
 	}
 	 _putchar('\n');

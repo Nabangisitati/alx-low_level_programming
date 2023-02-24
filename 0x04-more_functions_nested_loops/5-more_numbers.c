f6d2271fd61ce9d10b5c4c163e61a73c59683cc2
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-24 06:29:55 -0800

    print 0 to 14 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 to 14
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i <= 9)
+			{
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar((i / 10) + '0');
+			}
+		}
+	}
+	_putchar('\n');
+}

e081549aec167d75e627172a544f002bdd7fd4ff
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-28 11:02:13 -0800

    List elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array
+ * @a: pointer to an array
+ * @n: lenth of string
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d", *(a + i));
+		printf("%d, ", *(a + i));
+	}
+}

36f4ee33d3e295d61f4869eaac62a4c716643a2a
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-28 19:54:00 -0800

    Copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string into another
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to a string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*src[i] != '\0')
+	{
+		*dest[i] = *src[i];
+		i++;
+	}
+	*dest[i] = '\0';
+	return (dest);
+}

4e94e9b3b725ee6a9abf4d323ada9202f32d8b8a
Phyllis Sitati <nabangisitati@gmail.com>
2023-02-28 19:07:53 -0800

    Print an array in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int start = 0, temp, end, i = 0;
+
+	/*Find length of string s*/
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	 end = i - 1;
+
+	while (start < end)
+	{
+		temp = *(s + start);
+		*(s + start) = *(s + end);
+		*(s + end) = temp;
+		end--;
+		start++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		_putchar(*(s + j));
+	}
+	_putchar('\n');
+
+}
